a12826a7d3d68177dbc3da18343aa330c7b7a188
Proladium <132358525+Proladium@users.noreply.github.com>
2023-05-24 15:14:21 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,52 +1,113 @@
-// This is a basic outline for a platformer game in JavaScript.
+// Player class
+class Player {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.width = PLAYER_WIDTH;
+    this.height = PLAYER_HEIGHT;
+    this.speed = PLAYER_SPEED;
+  }
 
-// First, we need to set up our game environment. This will include the game canvas and the game loop.
+  move(keys) {
+    if (keys['ArrowLeft']) {
+      this.vx = -this.speed;
+    } else if (keys['ArrowRight']) {
+      this.vx = this.speed;
+    } else {
+      this.vx = 0;
+    }
 
-// The canvas is the area where our game will be displayed. We'll create it using the HTML5 canvas API.
-var canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-canvas.width = 800;
-canvas.height = 600;
+    if (keys['ArrowUp'] && this.y === 0) {
+      this.vy = -this.speed * 2;
+    }
 
-// The context is what we'll use to draw on the canvas.
-var ctx = canvas.getContext('2d');
+    this.vy += 1;
+    this.x += this.vx;
+    this.y += this.vy;
 
-// The game loop is a function that updates the game state and redraws the screen.
-// We'll use requestAnimationFrame to call it as fast as the browser can handle (usually 60 times per second).
-function gameLoop() {
-  // Update game state
-  
-  // Redraw screen
-  
-  // Call the next game loop
-  requestAnimationFrame(gameLoop);
+    if (this.y > 0) {
+      this.y = 0;
+      this.vy = 0;
+    }
+  }
+
+  draw(ctx, camera) {
+    ctx.fillRect(this.x, CANVAS_HEIGHT - this.height - this.y - camera.y, this.width, this.height);
+  }
 }
 
-// Start the game loop
-requestAnimationFrame(gameLoop);
+// Object class
+class FallingObject {
+  constructor() {
+    this.x = Math.random() * CANVAS_WIDTH;
+    this.y = CANVAS_HEIGHT;
+    this.vy = Math.random() * (OBJECT_SPEED_MAX - OBJECT_SPEED_MIN) + OBJECT_SPEED_MIN;
+    this.width = OBJECT_WIDTH;
+    this.height = OBJECT_HEIGHT;
+  }
 
-// Now we need to create our player. The player will be an object with properties for its position and velocity.
-var player = {
-  x: 0,
-  y: 0,
-  vx: 0,
-  vy: 0
-};
+  move() {
+    this.y += this.vy;
+  }
 
-// We'll also need to handle input. We'll create an object to hold the current state of each key.
-var keys = {};
-window.addEventListener('keydown', function(e) {
-  keys[e.key] = true;
-});
-window.addEventListener('keyup', function(e) {
-  keys[e.key] = false;
-});
+  draw(ctx, camera) {
+    ctx.fillRect(this.x, this.y - camera.y, this.width, this.height);
+  }
+}
 
-// In our game loop, we'll update the player's velocity based on which keys are pressed.
-// Then we'll update the player's position based on its velocity.
+// Camera class
+class Camera {
+  constructor() {
+    this.y = 0;
+  }
 
-// We'll also need to implement collision detection. This will involve checking if the player is colliding with any platforms or enemies, and responding appropriately.
+  update(player) {
+    this.y = Math.max(this.y, player.y);
+  }
+}
 
-// Finally, we'll need to draw everything to the screen. This will involve clearing the canvas, then drawing the player and any platforms or enemies.
+// Game class
+class Game {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+    this.canvas.width = CANVAS_WIDTH;
+    this.canvas.height = CANVAS_HEIGHT;
+    this.ctx = this.canvas.getContext('2d');
 
-// This is just a basic outline. There are many more details you'll need to fill in as you go. Good luck!
+    this.player = new Player();
+    this.objects = [];
+    this.camera = new Camera();
+
+    this.keys = {};
+    window.addEventListener('keydown', (e) => this.keys[e.key] = true);
+    window.addEventListener('keyup', (e) => this.keys[e.key] = false);
+
+    setInterval(() => this.objects.push(new FallingObject()), SPAWN_INTERVAL);
+  }
+
+  loop() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.player.move(this.keys);
+    this.player.draw(this.ctx, this.camera);
+
+    for (let object of this.objects) {
+      object.move();
+      object.draw(this.ctx, this.camera);
+    }
+
+    this.camera.update(this.player);
+
+    requestAnimationFrame(() => this.loop());
+  }
+
+  start() {
+    this.loop();
+  }
+}
+
+// Start the game
+new Game().start();
